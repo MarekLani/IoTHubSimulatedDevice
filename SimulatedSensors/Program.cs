587873827243ef50137a238868f3adf8d02e7fc2
Marek <mareklani@gmail.com>
2019-04-15 16:22:31 +0200

    Initial Commit

---
+++ SimulatedSensors/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Devices.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulatedSensors
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var r = new Random();
+            var iothubDC = DeviceClient.CreateFromConnectionString("{UseYourDeviceConnectionStringHere}");
+
+            while (true)
+            { 
+                var messageString = "{\"time\":\""+DateTime.Now+"\",\"temperature\":" + r.Next(10).ToString() + ",\"flow\":" + r.Next(10).ToString() + "}";
+                Console.WriteLine("Sending message:" + messageString);
+                Message iotHubMessage = new Message(Encoding.UTF8.GetBytes(messageString));
+                await iothubDC.SendEventAsync(iotHubMessage);
+                await Task.Delay(5000);
+            }
+        }
+    }
+}
